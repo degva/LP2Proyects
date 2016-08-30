108ce603669040a1132caf3d2c2b3da70ed9fe4b
fabricio21x <fabricio2111@gmail.com>
2016-08-29 19:42:38 -0500

    asd

---
+++ ConsoleApplication1/ConsoleApplication1/Modelo/ParticipantePucp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Modelo
+{
+    public class ParticipantePucp : EquipoParticipante
+    {
+
+        private List<MiembroPucp> _equipoPucp;
+
+        public ParticipantePucp(string tema,
+            string fechaParticipacion,
+            int horaParticipacion,
+            string nombreArchivo)
+            : base(tema, fechaParticipacion, horaParticipacion, nombreArchivo)
+        {
+            _equipoPucp = new List<MiembroPucp>();
+        }
+
+
+        public void AgregarMiembroPucp(MiembroPucp miembroPucp)
+        {
+            _equipoPucp.Add(miembroPucp);
+        }
+
+
+        public void GetNombresIntegrantes()
+        {
+            Console.WriteLine("Integrantes:");
+            int counter = 1;
+            foreach (var item in _equipoPucp)
+            {
+                if (counter == 1)
+                {
+                    Console.Write(item.Nombre);
+                }
+                else
+                {
+                    Console.WriteLine("," + item.Nombre);
+                }
+            }
+        }
+    }
+}
