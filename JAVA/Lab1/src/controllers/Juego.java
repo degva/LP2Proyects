a695b1631aa0fa2d97b0b8a7d515203a4c01385c
Fabricio Monsalve <fabricio2111@gmail.com>
2016-09-06 23:11:49 -0500

    lel

--- JAVA/Lab1/src/controllers/Juego.java
+++ JAVA/Lab1/src/controllers/Juego.java
@@ -93,7 +93,7 @@ public class Juego {
                     continue;
                 }                
             }
-            else if (sigAnt == -1 && laberintoActual != 0 ){
+            else if (sigAnt == -1 && laberintoActual != laberintoAnterior){
                 System.out.println("Pasando a nivel anterior");
                 laberintoActual--;
                 // cuando se va a otro laberinto usualmente se loquea y se pone sobre un muro :v
