5a0ffd6ad553197baa133dbbef7e72f7b3d2a199
Ginx <g.bustamante1303@gmail.com>
2016-09-10 17:15:36 -0500

    Tarea semana 4

---
+++ C#/Ejercicios de clase - Semana 4/Proyecto/ConsoleApplication1/ConsoleApplication1/Modelo/MiembroPucp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Modelo
+{
+    public class MiembroPucp
+    {
+        private string codigoPucp;
+        private string dni;
+        private string nombre;
+        private string fechaNacimiento;
+        private string correo;
+        private int tipoOrganizador; // 1 es organizador - 0 no es organizador
+        private string cargo;
+        private string responsabilidades;
+        private int rol;
+
+        public string CodigoPucp
+        {
+            get { return codigoPucp; }
+            set { codigoPucp = value; }
+        }
+
+        public string Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public string Nombre
+        {
+            get {return nombre; }
+            set {nombre = value; }
+        }
+
+        public string FechaNacimiento
+        {
+            get {return fechaNacimiento; }
+            set {fechaNacimiento = value; }
+        }
+
+        public string Correo
+        {
+            get {return correo; }
+            set {correo = value; }
+        }
+
+        public int TipoOrganizador
+        {
+            get {return tipoOrganizador; }
+            set {tipoOrganizador = value; }
+        }
+
+        public string Cargo
+        {
+            get {return cargo; }
+            set {cargo = value; }
+        }
+
+        public string Responsabilidades
+        {
+            get {return responsabilidades; }
+            set {responsabilidades = value; }
+        }
+
+
+        public MiembroPucp(string codigo, int rol) //rol = 1 para alumno y rol = 2 para trabajador
+        {
+            codigoPucp = codigo;
+            this.rol = rol;
+        }
+    }
+}
