83d393d92caa8b5e03abf6d33c13e89c5457db75
Diego Vargas <degva.29@gmail.com>
2016-09-14 06:36:16 -0500

    New shit

---
+++ C#/PracticasPasadas/PC2-20132/PC2-20132/Modelo/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC2_20132.Modelo
+{
+    class Persona
+    {
+        private int _id;
+        private String _nombre;
+
+        public Persona(int id, String nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+
+        public String ACadena()
+        {
+            String aux = "Persona Id: " + Id.ToString() +"\nNombre: " + Nombre;
+            return aux;
+        }
+
+        public String Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+    }
+}
---
+++ C#/PracticasPasadas/PC2-20132/PC2-20132/Modelo/PersonaJuridica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC2_20132.Modelo
+{
+    class PersonaJuridica : Persona
+    {
+        private String _ruc;
+
+        public PersonaJuridica(int id, String razonSocial, String ruc)
+            : base(id, razonSocial)
+        {
+            Ruc = ruc;
+        }
+
+        public String RazonSocial
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+
+        public String Ruc
+        {
+            get { return _ruc; }
+            set { _ruc = value; }
+        }
+
+    }
+}
