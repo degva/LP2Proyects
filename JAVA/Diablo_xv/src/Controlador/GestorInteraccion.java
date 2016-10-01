582af6015f110497267543893f2d5c12a046ad8b
Fabricio Monsalve <fabricio2111@gmail.com>
2016-10-01 15:14:05 -0500

    reverting

--- JAVA/Diablo_xv/src/Controlador/GestorInteraccion.java
+++ JAVA/Diablo_xv/src/Controlador/GestorInteraccion.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class GestorInteraccion {
     
     private GestorAvatar _gestorAvatar;
+    
     public GestorInteraccion(){
         _gestorAvatar = new GestorAvatar();
     }
