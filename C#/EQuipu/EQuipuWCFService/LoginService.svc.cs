53e2ea0ff28e823d9808d64579651939488b9cfc
Diego Vargas <degva.29@gmail.com>
2016-11-25 05:38:58 -0500

    Added the WCF Service

---
+++ C#/EQuipu/EQuipuWCFService/LoginService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EQuipuWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LoginService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select LoginService.svc or LoginService.svc.cs at the Solution Explorer and start debugging.
+    public class LoginService : ILoginService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
