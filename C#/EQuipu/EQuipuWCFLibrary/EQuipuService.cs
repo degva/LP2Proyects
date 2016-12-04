71e34f1ddab5baa16296538b3314710e92a223d4
Diego Vargas <degva.29@gmail.com>
2016-12-04 13:26:43 -0500

    Changes

--- C#/EQuipu/EQuipuWCFLibrary/EQuipuService.cs
+++ C#/EQuipu/EQuipuWCFLibrary/EQuipuService.cs
@@ -5,18 +5,37 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+using MySql.Data.MySqlClient;
+
 namespace EQuipuWCFLibrary
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "LoginService" en el código y en el archivo de configuración a la vez.
-    public class LoginService : ILoginService
+    public class EquipuService : IEQuipuService
     {
         private List<Usuario> _lista_usuarios;
+        private MySqlConnection _con;
 
-        public LoginService()
+        public EquipuService()
         {
-            Usuario u = new Usuario("degva", "123");
+            _con = new MySqlConnection();
+
+            String pass = "F6700370";
+            _con.ConnectionString = "Server=192.168.200.13;Database=BDLP2_20114297;User=U20114297;Password=" + pass + ";";
+            _con.Open();
+
+            /* Para usar la conexion de la base de datos
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = "select * from CICLO_20161_Usuarios";
+            SqlDataReader reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                reader['nombredelacolumna']
+            }
+             */
+
+
             _lista_usuarios = new List<Usuario>();
-            _lista_usuarios.Add(u);
         }
 
         public bool ValidarUsuario(string username, string password)
--- C#/EQuipu/EQuipuWCFLibrary/IEQuipuService.cs
+++ C#/EQuipu/EQuipuWCFLibrary/IEQuipuService.cs
@@ -1,33 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EQuipuWCFLibrary
 {
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ILoginService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IEQuipuService
+    {
+        [OperationContract]
+        bool ValidarUsuario(string usuario, string contraseña);
+    }
+
+    [DataContract]
     public class Usuario
     {
+        [DataMember]
         private string _username;
+        [DataMember]
         private string _password;
 
+        [DataMember]
         public Usuario(string usr, string pass)
         {
             this._username = usr;
             this._password = pass;
         }
 
+        [DataMember]
         public Boolean Validate(string pass)
         {
             return this._password == pass;
         }
 
+        [DataMember]
         public string Username
         {
             get { return _username; }
             set { _username = value; }
         }
 
+        [DataMember]
         public string Password
         {
             get { return _password; }
